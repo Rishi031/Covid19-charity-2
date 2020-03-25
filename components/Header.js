fa3f6deb8219976d493738600d133b96fa7554e0
punisher03 <rshk70@gmail.com>
2020-03-26 05:24:44 +0530

    update

--- components/Header.js
+++ components/Header.js
@@ -6,16 +6,16 @@ export default () => {
   return (
     <Menu style={{ marginTop: '10px' }}>
       <Link href="https://hackcovid19-nit-rishi135.hub.arcgis.com/">
-        <a className="item">Statistics</a>
+        <a className="item">Symptoms and Precautions</a>
       </Link>
 
       <Menu.Menu position="right">
         <Link href="https://hackcovid19-nits.netlify.com/">
-          <a className="item">Symptoms and Precautions</a>
+          <a className="item">Statistics</a>
         </Link>
 
-        <Link>
-          <a className="item">+</a>
+        <Link route="/">
+          <a className="item">Home</a>
         </Link>
       </Menu.Menu>
     </Menu>
