327a5892e3be4547c46be15ef06ebddedc61e5f0
Rishi031 <62686341+Rishi031@users.noreply.github.com>
2020-06-11 23:32:12 +0530

    Update new.js

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -3,6 +3,7 @@ import { Form, Button, Message, Input } from 'semantic-ui-react';
 import Campaign from '../../../ethereum/campaign';
 import web3 from '../../../ethereum/web3';
 import { Link, Router } from '../../../routes';
+
 import Layout from '../../../components/Layout';
 
 class RequestNew extends Component {
