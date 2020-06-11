fb65e0ce0d08ed2f595403360cc43fc3af3f0013
Rishi031 <62686341+Rishi031@users.noreply.github.com>
2020-06-11 23:39:01 +0530

    Create StratisContract2.cs

---
+++ ethereum/contracts/StratisContract2.cs
@@ -0,0 +1,11 @@
+  
+using System;
+using Stratis.SmartContracts;
+
+public class MyContract : SmartContract
+{
+    public MyContract(ISmartContractState smartContractState)
+    : base(smartContractState)
+    {
+    }
+}
